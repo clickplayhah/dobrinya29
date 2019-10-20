49c59f4eccfd08f73fc48a9cae02c3d70441d613
Artik <mail@artik.me>
2019-10-20 23:49:52 +0300

    UPD: ESNEXT -> ES5 (TS); favicon

--- script.js
+++ script.js
@@ -1,158 +1,167 @@
-const global = {
-  lastFocus: null,
-  returnFocus: () => global.lastFocus && global.lastFocus.focus(),
-  firstFocusable: element =>
-    element.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])'),
-  getScrollbarWidth: () => {
-    const outer = document.createElement('div');
-    outer.style.visibility = 'hidden';
-    outer.style.overflow = 'scroll';
-    document.body.appendChild(outer);
-    const inner = document.createElement('div');
-    outer.appendChild(inner);
-    const scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
-    outer.parentNode.removeChild(outer);
-    if (document.body.scrollHeight > window.innerHeight) {
-      return scrollbarWidth;
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var tiny_slider_1 = require("./node_modules/tiny-slider/src/tiny-slider");
+var global = {
+    lastFocus: null,
+    returnFocus: function () {
+        if (this.lastFocus) {
+            this.lastFocus.focus();
+        }
+    },
+    firstFocusable: function (element) {
+        return element.querySelector('button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])');
+    },
+    getScrollbarWidth: function () {
+        var outer = document.createElement('div');
+        outer.style.visibility = 'hidden';
+        outer.style.overflow = 'scroll';
+        document.body.appendChild(outer);
+        var inner = document.createElement('div');
+        outer.appendChild(inner);
+        var scrollbarWidth = outer.offsetWidth - inner.offsetWidth;
+        if (outer.parentNode) {
+            outer.parentNode.removeChild(outer);
+        }
+        if (document.body.scrollHeight > window.innerHeight) {
+            return scrollbarWidth;
+        }
+        return 0;
     }
-    return 0;
-  }
 };
-
-class Modal {
-  constructor(modalId) {
-    this.id = modalId;
-    this.modal = document.querySelector(`[data-modal="${modalId}"]`);
-    this.closeButton = this.modal.querySelector('[data-close-modal]');
-
-    this.closeButton.addEventListener('click', event => {
-      this.close();
-    });
-
-    this.modal.addEventListener('click', event => {
-      if (event.target === this.modal) {
-        this.close();
-      }
-    });
-  }
-
-  open(initiator) {
-    this.modal.setAttribute('open', '');
-    document.body.style.overflow = 'hidden';
-    document.body.style.paddingRight = global.getScrollbarWidth() + 'px';
-    if (initiator) {
-      lastFocus = button;
-    }
-    const focusable = global.firstFocusable(this.modal);
-    if (focusable) {
-      focusable.focus();
-    }
-  }
-
-  close() {
-    this.modal.removeAttribute('open');
-    global.returnFocus();
-    document.body.style.overflow = 'auto';
-    document.body.style.paddingRight = 0;
-  }
-}
-
-(() => {
-  const modals = new Map();
-
-  document.querySelectorAll('[data-open-modal]').forEach(button => {
-    const modalId = button.dataset.openModal;
-    let modal;
-    if (modals.has(modalId)) {
-      modal = modals.get(modalId);
-    } else {
-      modal = new Modal(modalId);
-      modals.set(modal.id, modal);
-    }
-
-    button.addEventListener('click', () => {
-      modal.open();
-    });
-  });
-
-  document.addEventListener('keydown', event => {
-    if (event.key === 'Escape') {
-      modals.forEach(modal => {
-        modal.close();
-      });
-    }
-  });
-
-  const form = document.forms.form;
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-
-    const message = `${form.elements.name.value} (${form.elements.phone.value})\n ${form.elements.comment.value}`;
-    const url = 'https://dobrinya29bot.herokuapp.com/?message=' + encodeURI(message);
-
-    const error = () => {
-      modals.forEach(modal => {
-        modal.close();
-      });
-      const errorModal = new Modal('modal-error');
-      errorModal.open();
-      setTimeout(() => {
-        errorModal.close();
-      }, 5000);
-    };
-
-    const success = () => {
-      modals.forEach(modal => {
-        modal.close();
-      });
-      const successModal = new Modal('modal-success');
-      successModal.open();
-      setTimeout(() => {
-        successModal.close();
-      }, 3000);
-    };
-
-    try {
-      fetch(url)
-        .then(resp => resp.json())
-        .then(resp => {
-          if (resp && resp.status === 'OK') {
-            success();
-          } else {
-            error();
-          }
+var Modal = /** @class */ (function () {
+    function Modal(modalId) {
+        var _this = this;
+        this.id = modalId;
+        this.modal = document.querySelector("[data-modal=\"" + modalId + "\"]");
+        if (!this.modal) {
+            throw new Error('Modal element is not found');
+        }
+        this.modal.addEventListener('click', function (event) {
+            if (event.target === _this.modal) {
+                _this.close();
+            }
         });
-    } catch (e) {
-      error();
+        this.closeButton = this.modal.querySelector('[data-close-modal]');
+        if (this.closeButton) {
+            this.closeButton.addEventListener('click', function () {
+                _this.close();
+            });
+        }
     }
-  });
-
-  const slider = tns({
-    container: '.slider',
-    items: 1,
-    controls: false,
-    navPosition: 'bottom',
-    autoplay: true,
-    autoplayHoverPause: true,
-    autoplayButtonOutput: false,
-    mouseDrag: true,
-    responsive: {
-      600: {
-        items: 2
-      },
-      1000: {
-        items: 3
-      },
-      1400: {
-        items: 4
-      },
-      1800: {
-        items: 5
-      },
-      2200: {
-        items: 6
-      }
-    }
-  });
-  slider.play();
+    Modal.prototype.open = function (initiator) {
+        this.modal.setAttribute('open', '');
+        document.body.style.overflow = 'hidden';
+        document.body.style.paddingRight = global.getScrollbarWidth() + 'px';
+        if (initiator) {
+            global.lastFocus = initiator;
+        }
+        var focusable = global.firstFocusable(this.modal);
+        if (focusable) {
+            focusable.focus();
+        }
+    };
+    Modal.prototype.close = function () {
+        this.modal.removeAttribute('open');
+        global.returnFocus();
+        document.body.style.overflow = 'auto';
+        document.body.style.paddingRight = '0';
+    };
+    return Modal;
+}());
+(function () {
+    var modals = new Map();
+    document.querySelectorAll('[data-open-modal]').forEach(function (button) {
+        var modalId = button.dataset.openModal;
+        var modal;
+        if (modals.has(modalId)) {
+            modal = modals.get(modalId);
+        }
+        else {
+            modal = new Modal(modalId);
+            modals.set(modal.id, modal);
+        }
+        button.addEventListener('click', function () {
+            modal.open();
+        });
+    });
+    document.addEventListener('keydown', function (event) {
+        if (event.key === 'Escape') {
+            modals.forEach(function (modal) {
+                modal.close();
+            });
+        }
+    });
+    var form = document.forms[0];
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        var name = form.elements.namedItem('name');
+        var phone = form.elements.namedItem('phone');
+        var comment = form.elements.namedItem('comment');
+        var message = name.value + " (" + phone.value + ")\n " + comment.value;
+        var url = 'https://dobrinya29bot.herokuapp.com/?message=' + encodeURI(message);
+        var error = function () {
+            modals.forEach(function (modal) {
+                modal.close();
+            });
+            var errorModal = new Modal('modal-error');
+            errorModal.open();
+            setTimeout(function () {
+                errorModal.close();
+            }, 5000);
+        };
+        var success = function () {
+            modals.forEach(function (modal) {
+                modal.close();
+            });
+            var successModal = new Modal('modal-success');
+            successModal.open();
+            setTimeout(function () {
+                successModal.close();
+            }, 3000);
+        };
+        try {
+            fetch(url)
+                .then(function (resp) { return resp.json(); })
+                .then(function (resp) {
+                if (resp && resp.status === 'OK') {
+                    success();
+                }
+                else {
+                    error();
+                }
+            });
+        }
+        catch (e) {
+            error();
+        }
+    });
+    var slider = tiny_slider_1.tns({
+        container: '.slider',
+        items: 1,
+        controls: false,
+        // @ts-ignore
+        navPosition: 'bottom',
+        autoplay: true,
+        autoplayHoverPause: true,
+        autoplayButtonOutput: false,
+        mouseDrag: true,
+        responsive: {
+            600: {
+                items: 2
+            },
+            1000: {
+                items: 3
+            },
+            1400: {
+                items: 4
+            },
+            1800: {
+                items: 5
+            },
+            2200: {
+                items: 6
+            }
+        }
+    });
+    slider.play();
 })();
